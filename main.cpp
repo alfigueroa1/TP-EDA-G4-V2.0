66621a620d0b010a1b2379533fa17027766423fc
alfigueroa1 <alfigueroa@itba.edu.ar>
2019-09-10 15:23:08 -0300

    Anda todo perfectoooo

--- main.cpp
+++ main.cpp
@@ -29,23 +29,14 @@ int main(int argc, char** argv) {
 		int error = 0, quit = 0, line = 1;
 		int instance = 0;
 		eventType ev = NOEVENT;
-		//FILE* archivo;
 		stackFSMs[0] = &element;
-		/*archivo = fopen("JSONFile.txt", "r");
-		if (archivo == NULL) {
-			printf("Error al intentar abrir el archivo de Json\n");
-			return 0;
-		}*/
 
 		unsigned int stackLevel = 0, last = 0;
 		while (ev != EOF && !quit) {
-			//if (!flag)
 				ev = getNextEvent(archivo.file, line);
 			if (ev != NOEVENT) {
-				printf("Event detected: %c at line %d\n", ev, line);
-				//if(!flag)
+				//printf("Event detected: %c at line %d\n", ev, line);
 				stackFSMs[stackLevel]->cycle(&ev);
-				//flag = false;
 				if ((stackFSMs[stackLevel]->getState()) == ERROR) {
 					quit = 1;
 					printf("Error Found at line %d!\n", line);
@@ -64,8 +55,6 @@ int main(int argc, char** argv) {
 								stackFSMsPop(stackFSMs, stackLevel);
 						}
 						stackFSMs[stackLevel]->cycle(&ev);
-						//if((stackFSMs[stackLevel]->getState()) == FIN)
-						//	flag = true;
 						checkNewInstance(stackFSMs, stackLevel, &ev);
 					}
 				}
@@ -83,7 +72,7 @@ int main(int argc, char** argv) {
 
 	}
 	else
-		printf("Archivo invalido");
+		printf("Invalid File\n");
 	return 0;
 }
 
@@ -92,15 +81,15 @@ int main(int argc, char** argv) {
 void stackFSMsPush(int instance, genericFSM** stack, uint& stackLevel) {
 	genericFSM* newFSM = NULL;
 	switch (instance) {
-	case NEWELEM:	newFSM = new (std::nothrow) FSMElement;	printf("Entering Element FSM\n"); break;
-	case NEWOBJ:	newFSM = new (std::nothrow) FSMObject;	printf("Entering Object FSM\n"); break;
-	case NEWVALUE:	newFSM = new (std::nothrow) FSMValue;	printf("Entering Value FSM\n"); break;
-	case NEWARRAY: newFSM = new (std::nothrow) FSMArray;	printf("Entering Array FSM\n");	break;
-	case NEWSTRING: newFSM = new (std::nothrow) FSMString;	printf("Entering String FSM\n"); break;
-	case NEWNUM: newFSM = new (std::nothrow) FSMNumber;		printf("Entering Number FSM\n"); break;
-	case NEWTRUE: newFSM = new (std::nothrow) FSMTrue;		printf("Entering True FSM\n"); break;
-	case NEWFALSE: newFSM = new (std::nothrow) FSMFalse;	printf("Entering False FSM\n"); break;
-	case NEWNULL: newFSM = new (std::nothrow) FSMNull;		printf("Entering Null FSM\n"); break;
+	case NEWELEM:	newFSM = new (std::nothrow) FSMElement;	/*printf("Entering Element FSM\n");*/ break;
+	case NEWOBJ:	newFSM = new (std::nothrow) FSMObject;	/*printf("Entering Object FSM\n");*/ break;
+	case NEWVALUE:	newFSM = new (std::nothrow) FSMValue;	/*printf("Entering Value FSM\n");*/ break;
+	case NEWARRAY: newFSM = new (std::nothrow) FSMArray;	/*printf("Entering Array FSM\n");*/	break;
+	case NEWSTRING: newFSM = new (std::nothrow) FSMString;	/*printf("Entering String FSM\n");*/ break;
+	case NEWNUM: newFSM = new (std::nothrow) FSMNumber;		/*printf("Entering Number FSM\n"); */ break;
+	case NEWTRUE: newFSM = new (std::nothrow) FSMTrue;		/*printf("Entering True FSM\n");*/ break;
+	case NEWFALSE: newFSM = new (std::nothrow) FSMFalse;	/*printf("Entering False FSM\n");*/ break;
+	case NEWNULL: newFSM = new (std::nothrow) FSMNull;		/*printf("Entering Null FSM\n");*/ break;
 	default: genericFSM* newFSM = NULL; break;
 	}
 	if (newFSM == NULL) {
@@ -117,8 +106,8 @@ void stackFSMsPop(genericFSM** stack, uint& stackLevel) {
 		delete stack[stackLevel];
 		stackLevel--;
 	}
-	printf("Instance finished, stack poped\n");
-	printf("StackLevel: %d\n", stackLevel);
+	//printf("Instance finished, stack poped\n");
+	//printf("StackLevel: %d\n", stackLevel);
 }
 
 void freeStack(genericFSM** stack, uint& stackLevel) {
@@ -133,9 +122,8 @@ void checkNewInstance(genericFSM** stackFSMs, uint& stackLevel, eventType *ev) {
 		|| (stackFSMs[stackLevel]->getState()) == NEWVALUE || (stackFSMs[stackLevel]->getState()) == NEWSTRING || (stackFSMs[stackLevel]->getState()) == NEWNUM) {
 		stackFSMsPush(stackFSMs[stackLevel]->getState(), stackFSMs, stackLevel);
 		//printf("New FSM instance needed!\n");
-		printf("StackLevel: %d\n", stackLevel);
-		//if(stackFSMs[stackLevel-1]->getDone() == false)
-			stackFSMs[stackLevel]->cycle(ev);
+		//printf("StackLevel: %d\n", stackLevel);
+		stackFSMs[stackLevel]->cycle(ev);
 	}
 	return;
 }
